a931606832770af8cb63549b195625ac08dc525e
Rowan Oulton <rowan.oulton@gmail.com>
2015-03-27 18:01:59 +0100

    Initial implementation of game model on DOM load

--- src/js/bowler.js
+++ src/js/bowler.js
@@ -1,1 +1,19 @@
-var Frame = require('./models/frame.js');
+var Game = require('./models/game'),
+    bowlingGame,
+    onLoad;
+
+// Create instance of bowling game
+bowlingGame = new Game();
+
+/**
+ * Set up bowling game on document load
+ * @method onLoad
+ */
+onLoad = function () {
+    // Initialise bowling game
+    bowlingGame.init();
+
+    console.log(bowlingGame);
+};
+
+document.addEventListener('DOMContentLoaded', onLoad);
